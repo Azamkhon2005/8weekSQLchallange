e7f1ffeef22fe16ae04b7463eed9634b8843e4f3
Azamkhon Buzurukov <89506742+Azamkhon2005@users.noreply.github.com>
2023-07-16 11:09:56 +0300

    Delete 3.first item purchased by customer.sql

--- case #1 - Danny's dinner/3.first item purchased by customer.sql
+++
@@ -1,8 +0,0 @@
--- What was the first item from the menu purchased by each customer?
-
-select customer_id, product_name from (
-	select customer_id, product_id,
-		row_number() over(partition by  customer_id order by order_date) as first_time
-		from sales) as t1
-inner join menu using(product_id)
-where first_time = 1
