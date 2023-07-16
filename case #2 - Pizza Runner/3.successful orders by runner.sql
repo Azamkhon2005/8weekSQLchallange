0fab02309875011e95b1ff43d4e74e26d1bed915
Azamkhon Buzurukov <89506742+Azamkhon2005@users.noreply.github.com>
2023-07-16 11:18:30 +0300

    Delete case #2 - Pizza Runner directory

--- case #2 - Pizza Runner/3.successful orders by runner.sql
+++
@@ -1,5 +0,0 @@
--- How many successful orders were delivered by each runner?
-
-select runner_id,count(order_id) as orders from runner_orders  
-	where cancellation is null 
-	group by runner_id
--- case #2 - Pizza Runner/6.max num of pizzas.sql
+++
@@ -1,7 +0,0 @@
--- What was the maximum number of pizzas delivered in a single order?
-
-select order_id,count(order_id) as total_pizzas from customer_orders
-	where order_id not in (select order_id from runner_orders where cancellation is not null)
-    group by order_id
-    order by 2 desc
-    limit 1
