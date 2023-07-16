50264fc9bde7668b9edd7bb1840f617a2dacd073
Azamkhon Buzurukov <89506742+Azamkhon2005@users.noreply.github.com>
2023-07-16 11:09:32 +0300

    Delete 2.visited days by customer.sql

--- case #1 - Danny's dinner/2.visited days by customer.sql
+++
@@ -1,5 +0,0 @@
--- How many days has each customer visited the restaurant?
-
-select customer_id, count( distinct order_date) as visited_days from sales
-	group by customer_id
-    order by visited_days desc
