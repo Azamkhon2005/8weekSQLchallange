b5c71ecfe89d1bd6f4e0982fc8d14b46365019ae
Azamkhon Buzurukov <89506742+Azamkhon2005@users.noreply.github.com>
2023-07-16 11:10:38 +0300

    Delete 8.total items and amount before membership.sql

--- case #1 - Danny's dinner/8.total items and amount before membership.sql
+++
@@ -1,5 +0,0 @@
-select sales.customer_id, count(product_name) as total_items ,sum(price) as amount_spent from sales
-		inner join members using(customer_id)
-        inner join menu using(product_id)
-		where order_date<join_date
-        group by sales.customer_id
