f30c2ab88ffe349c216885ae02f6194e33db5fac
Azamkhon Buzurukov <89506742+Azamkhon2005@users.noreply.github.com>
2023-07-16 11:09:44 +0300

    Delete 9.total points by customer.sql

--- case #1 - Danny's dinner/9.total points by customer.sql
+++
@@ -1,9 +0,0 @@
-select customer_id,sum(points) as total_points from(
-	select customer_id,price,
-	case 
-		when product_name = 'sushi' then price*20
-		else price * 10
-		end as points
-	from sales
-	left join menu using(product_id) ) as t1
-group by customer_id
